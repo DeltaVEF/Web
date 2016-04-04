a9a04a3fb1903b5f6f59623901bd835461875630
Villermen <villermen@gmail.com>
2016-04-04 14:32:37 +0200

    Learning how to dependency and trying to make errors show

--- src/record.js
+++ src/record.js
@@ -1,13 +1,39 @@
 import {inject} from "aurelia-framework";
-import {HttpClient} from "aurelia-fetch-client";
+import {HttpClient, json} from "aurelia-fetch-client";
 import "fetch";
 
 @inject(HttpClient)
 export class Record {
   constructor(http) {
+		http.configure(config => {
+      config
+        .useStandardConfiguration()
+        .withBaseUrl("https://sjoadfeoyor.com/");
+    });
+
 		this.http = http;
   }
 
 	activate() {
+		this.http.fetch("room/create", {
+			method: "post",
+			body: json()
+		}).then(
+			response => {
+				this.room = response.json();
+			},
+			error => {
+				this.showError("Could not create room!");
+			}
+		);
+	}
+
+	attached() {
+		showError("Error?");
+	}
+
+	showError(message) {
+		$(".error-message").text(message);
+		$(".error-container").fadeIn(500);
 	}
 }
